2a7b9e81cf4f5366dd893091478686034434a782
Francisval Cavalcnate <f.tec1@hotmail.com>
2015-12-16 17:43:02 -0300

    Commit pizzadroid

---
+++ app/src/main/java/br/senac/pi/diskpizza/MenuActivity.java
@@ -0,0 +1,46 @@
+package br.senac.pi.diskpizza;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.btnSabores).setOnClickListener(abrirPedidos());
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    private View.OnClickListener abrirPedidos(){
+        return new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, PedidoActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+
+}
